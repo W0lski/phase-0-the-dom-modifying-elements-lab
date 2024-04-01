af994f088bdb37907b09abb0aa388e08685340bc
Wol <woldut101@icloud.com>
2024-04-01 14:12:21 +0300

    Adds Phase-0 the DOM Modifying Elements Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Wol is the champion";
+const body = document.querySelector('body');
+body.append(newHeader);
